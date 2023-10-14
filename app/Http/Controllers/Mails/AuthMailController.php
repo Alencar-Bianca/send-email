a2b36b5d8bad8c9d60e7d9f093cfbffaebb4a6c6
alencar-bianca <alencar-bianca@hotmail.com>
2023-10-14 18:24:41 -0300

    send email basic

--- app/Http/Controllers/Mails/AuthMailController.php
+++ app/Http/Controllers/Mails/AuthMailController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers\Mails;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Mail\RegisterMail;
+use Illuminate\Support\Facades\Mail;
 
 class AuthMailController extends Controller
 {
-    public function sendEmail() {
+    public function sendRegisterMail() {
+        $mail = new RegisterMail();
 
+        Mail::to('bianca.re4@hotmail.com')->send($mail);
     }
 }
