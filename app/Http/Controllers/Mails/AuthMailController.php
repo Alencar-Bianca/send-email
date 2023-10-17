ef0630e1faaaf2e189592389074579d42670c082
alencar-bianca <alencar-bianca@hotmail.com>
2023-10-16 21:46:22 -0300

    email dinamiocos

--- app/Http/Controllers/Mails/AuthMailController.php
+++ app/Http/Controllers/Mails/AuthMailController.php
@@ -5,14 +5,20 @@ namespace App\Http\Controllers\Mails;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Mail\RegisterMail;
+use App\Models\User;
 use Illuminate\Support\Facades\Mail;
 
 class AuthMailController extends Controller
 {
     public function sendRegisterMail() {
-        $mail = new RegisterMail();
+        // $mail = new RegisterMail();
 
-        // return $mail;
-        Mail::to('bianca.re4@hotmail.com')->send($mail);
+        $user = new User();
+        $user->name = 'regina';
+
+        $registerMail = new RegisterMail($user);
+
+        // return  $registerMail;
+        Mail::to('bianca.re4@hotmail.com')->send($registerMail);
     }
 }
